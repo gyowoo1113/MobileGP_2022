d886819c1f6726f15605705f3187380628c36a20
gyowoo1113 <yereube@naver.com>
2022-03-21 16:38:19 +0900

    선택 시 Ace Image, 이전 선택카드가 존재하면 그 카드를 backImage로 전환

--- Practice/CardApps/app/src/main/java/kr/ac/tukorea/ge/sgp02/a2019182019/cardapps/MainActivity.java
+++ Practice/CardApps/app/src/main/java/kr/ac/tukorea/ge/sgp02/a2019182019/cardapps/MainActivity.java
@@ -42,6 +42,10 @@ public class MainActivity extends AppCompatActivity {
         int btnIndex = findButtonIndex(imageButton.getId());
         Log.d(TAG,"onBtnCard" + btnIndex);
 
+        imageButton.setImageResource(R.mipmap.card_as);
+        if(previousButton != null){
+            previousButton.setImageResource(R.mipmap.card_blue_back);
+        }
         previousButton = imageButton;
     }
 
