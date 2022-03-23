6354be14d4a6248584c6d04be1ba23ce9a0ceff3
gyowoo1113 <yereube@naver.com>
2022-03-23 13:50:46 +0900

    Checkbox 추가

--- Practice/MoreControls/app/src/main/java/kr/ac/tukorea/ge/sgp02/a2019182019/morecontrols/MainActivity.java
+++ Practice/MoreControls/app/src/main/java/kr/ac/tukorea/ge/sgp02/a2019182019/morecontrols/MainActivity.java
@@ -3,12 +3,26 @@ package kr.ac.tukorea.ge.sgp02.a2019182019.morecontrols;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.CheckBox;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private CheckBox checkbox;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        checkbox = findViewById(R.id.checkbox);
+    }
+
+    public void onCheckBox(View view) {
+        CheckBox cb = (CheckBox) view;
+        Log.d(TAG,"Checkbox state: " +cb.isChecked());
+    }
+
     }
 }
