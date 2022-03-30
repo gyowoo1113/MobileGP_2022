a1b49d66cc9b4c5c373a75eb3006bc12eb71c759
gyowoo1113 <yereube@naver.com>
2022-03-30 22:24:29 +0900

    Remove: 사용하지 않는 실습용 함수, 변수 제거

--- Practice/MoreControls/app/src/main/java/kr/ac/tukorea/ge/sgp02/a2019182019/morecontrols/GameView.java
+++ Practice/MoreControls/app/src/main/java/kr/ac/tukorea/ge/sgp02/a2019182019/morecontrols/GameView.java
@@ -20,7 +20,6 @@ public class GameView extends View {
     private Rect soccerSrcRect = new Rect();
     private Rect soccerDstRect = new Rect();
     private Paint leftCirclePaint = new Paint();
-    private Paint rightCirclePaint = new Paint();
     private Paint textPaint = new Paint();
     private Rect textExtentRect = new Rect();
 
@@ -32,10 +31,6 @@ public class GameView extends View {
     private void initView() {
         paint.setColor(0xFFCCCCCC);
         leftCirclePaint.setColor(Color.BLUE);
-        rightCirclePaint.setColor(Color.RED);
-        rightCirclePaint.setStyle(Paint.Style.STROKE);
-        rightCirclePaint.setStrokeWidth(10);
-        textPaint.setColor(Color.GREEN);
         textPaint.setColor(Color.WHITE);
         Resources res = getResources();
         soccerBitmap = BitmapFactory.decodeResource(res,R.mipmap.soccer_ball_240);
@@ -45,7 +40,6 @@ public class GameView extends View {
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        //Paint paint = new Paint();
         int width = getWidth();
         int height = getHeight();
         int paddingLeft = getPaddingLeft();
@@ -63,8 +57,6 @@ public class GameView extends View {
         textPaint.setTextSize(size/20);
 
         drawBackGround(canvas, paddingLeft, paddingTop, contentWidth, contentHeight);
-        drawSoccerBall(canvas, size, centerX, centerY);
-        drawLeftCircle(canvas, paddingLeft, paddingTop, contentWidth, contentHeight, size);
         drawRightCircle(canvas, paddingTop, contentWidth, contentHeight, centerX, size);
         drawCenterText(canvas, contentHeight, centerX, centerY);
     }
@@ -73,18 +65,8 @@ public class GameView extends View {
         canvas.drawRoundRect(paddingLeft, paddingTop, paddingLeft + contentWidth, paddingTop + contentHeight,30,30,paint);
     }
 
-    private void drawSoccerBall(Canvas canvas, int size, int centerX, int centerY) {
-        int ballRadius = size /10;
-        soccerDstRect.set(centerX - ballRadius, centerY - ballRadius,
-                centerX + ballRadius, centerY + ballRadius);
-        canvas.drawBitmap(soccerBitmap,soccerSrcRect, soccerDstRect, null);
     }
 
-    private void drawLeftCircle(Canvas canvas, int paddingLeft, int paddingTop, int contentWidth, int contentHeight, int size) {
-        int leftCenterX = paddingLeft + contentWidth / 4;
-        int leftCenterY = paddingTop + contentHeight / 4;
-        int circleRadius = size / 16;
-        canvas.drawCircle(leftCenterX,leftCenterY,circleRadius,leftCirclePaint);
     }
 
     private void drawRightCircle(Canvas canvas, int paddingTop, int contentWidth, int contentHeight, int centerX, int size) {
