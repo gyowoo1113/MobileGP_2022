67aebcda09e926bb745cc3af957c180e96bd3334
gyowoo1113 <yereube@naver.com>
2022-04-01 15:27:38 +0900

    Feat: GameView 추가

---
+++ Practice/SampleGame/app/src/main/java/kr/ac/tukorea/ge/sgp02/a2019182019/samplegame/GameView.java
@@ -0,0 +1,19 @@
+package kr.ac.tukorea.ge.sgp02.a2019182019.samplegame;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.util.AttributeSet;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+
+public class GameView extends View {
+    public GameView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+    }
+}
