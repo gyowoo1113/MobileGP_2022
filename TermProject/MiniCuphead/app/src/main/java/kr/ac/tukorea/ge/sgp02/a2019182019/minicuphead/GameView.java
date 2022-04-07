7972d7f8f0bd9300498b8f200b2fe326687ea898
gyowoo1113 <yereube@naver.com>
2022-04-07 22:50:28 +0900

    Feat #2: 메인게임에 사용할 GameView, MainGame class 추가

---
+++ TermProject/MiniCuphead/app/src/main/java/kr/ac/tukorea/ge/sgp02/a2019182019/minicuphead/GameView.java
@@ -0,0 +1,11 @@
+package kr.ac.tukorea.ge.sgp02.a2019182019.minicuphead;
+
+import android.content.Context;
+import android.view.View;
+
+public class GameView extends View {
+
+    public GameView(Context context) {
+        super(context);
+    }
+}
---
+++ TermProject/MiniCuphead/app/src/main/java/kr/ac/tukorea/ge/sgp02/a2019182019/minicuphead/MainGame.java
@@ -0,0 +1,4 @@
+package kr.ac.tukorea.ge.sgp02.a2019182019.minicuphead;
+
+public class MainGame {
+}
