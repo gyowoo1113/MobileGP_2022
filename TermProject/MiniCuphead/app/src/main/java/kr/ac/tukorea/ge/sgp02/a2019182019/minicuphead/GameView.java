016c561b4256efde1b49c080a3948782196fd484
gyowoo1113 <yereube@naver.com>
2022-04-10 19:15:29 +0900

    Feat #2: GameView onDraw, doFrame, initView 선언

--- TermProject/MiniCuphead/app/src/main/java/kr/ac/tukorea/ge/sgp02/a2019182019/minicuphead/GameView.java
+++ TermProject/MiniCuphead/app/src/main/java/kr/ac/tukorea/ge/sgp02/a2019182019/minicuphead/GameView.java
@@ -1,11 +1,39 @@
 package kr.ac.tukorea.ge.sgp02.a2019182019.minicuphead;
 
 import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.util.AttributeSet;
+import android.view.Choreographer;
 import android.view.View;
 
-public class GameView extends View {
+import androidx.annotation.Nullable;
 
-    public GameView(Context context) {
-        super(context);
+public class GameView extends View implements Choreographer.FrameCallback{
+
+    public static GameView view;
+
+    public GameView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    private void initView() {
+        view = this;
+
+        MainGame game = MainGame.getInstance();
+        game.init();
+
+        Choreographer.getInstance().postFrameCallback(this);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        MainGame.getInstance().draw(canvas);
+    }
+
+    @Override
+    public void doFrame(long l) {
+
     }
 }
