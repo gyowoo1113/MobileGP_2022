35367e9ec0e670cd8fd018f09e8643996155d02d
gyowoo1113 <yereube@naver.com>
2022-04-10 19:15:01 +0900

    Feat #2 : MainGame init, update, draw 선언

--- TermProject/MiniCuphead/app/src/main/java/kr/ac/tukorea/ge/sgp02/a2019182019/minicuphead/MainGame.java
+++ TermProject/MiniCuphead/app/src/main/java/kr/ac/tukorea/ge/sgp02/a2019182019/minicuphead/MainGame.java
@@ -1,4 +1,33 @@
 package kr.ac.tukorea.ge.sgp02.a2019182019.minicuphead;
 
+import android.graphics.Canvas;
+import android.view.MotionEvent;
+
+import java.util.ArrayList;
+import java.util.Random;
+
 public class MainGame {
+    public static MainGame getInstance() {
+        if (singleton == null) {
+            singleton = new MainGame();
+        }
+        return singleton;
+    }
+
+    private MainGame() {
+
+    }
+
+    private static MainGame singleton;
+
+    public void init() {
+
+    }
+
+    public void update(int elapsedNanos) {
+
+    }
+
+    public void draw(Canvas canvas) {
+    }
 }
