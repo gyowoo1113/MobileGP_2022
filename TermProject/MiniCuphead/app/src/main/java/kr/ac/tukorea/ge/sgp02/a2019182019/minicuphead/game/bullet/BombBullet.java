9c7fd1e1b47556f89a6de34bf9a31fbd0e18397e
gyowoo1113 <yereube@naver.com>
2022-04-27 01:02:10 +0900

    Feat #5 : 대포 공격을 포물선 궤도로 변경

--- TermProject/MiniCuphead/app/src/main/java/kr/ac/tukorea/ge/sgp02/a2019182019/minicuphead/game/bullet/BombBullet.java
+++ TermProject/MiniCuphead/app/src/main/java/kr/ac/tukorea/ge/sgp02/a2019182019/minicuphead/game/bullet/BombBullet.java
@@ -8,11 +8,14 @@ import kr.ac.tukorea.ge.sgp02.a2019182019.minicuphead.framework.Metrics;
 import kr.ac.tukorea.ge.sgp02.a2019182019.minicuphead.game.MainGame;
 
 public class BombBullet extends Bullet {
+
+    public final float gravity;
     public BombBullet(float x, float y) {
         super(x, y);
         this.length = Metrics.size(R.dimen.laser_length);
         this.dx = -Metrics.size(R.dimen.laser_speed);
-        this.dy = -Metrics.size(R.dimen.laser_speed);
+        this.dy = Metrics.size(R.dimen.bullet_upper_speed);
+        gravity = Metrics.size(R.dimen.bullet_gravity);
 
         if (paint == null) {
             paint = new Paint();
@@ -27,6 +30,7 @@ public class BombBullet extends Bullet {
         float frameTime = MainGame.getInstance().frameTime;
         x -= dx * frameTime;
         y -= dy * frameTime;
+        dy -=gravity*frameTime;
 
         float hw = laserWidth / 2;
         boundingRect.set(x , y - hw, x - length, y + hw);
