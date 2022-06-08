f52353e7ca5ed5432ba9275bf282c925e8642c38
gyowoo1113 <yereube@naver.com>
2022-06-08 21:05:31 +0900

    Feat : 결과화면 애니메이션 시작시키기

--- TermProject/MiniCuphead/app/src/main/java/kr/ac/tukorea/ge/sgp02/a2019182019/minicuphead/app/ResultActivity.java
+++ TermProject/MiniCuphead/app/src/main/java/kr/ac/tukorea/ge/sgp02/a2019182019/minicuphead/app/ResultActivity.java
@@ -1,14 +1,30 @@
 package kr.ac.tukorea.ge.sgp02.a2019182019.minicuphead.app;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
+import android.widget.ImageView;
+
 import kr.ac.tukorea.ge.sgp02.a2019182019.minicuphead.R;
 
 public class ResultActivity extends AppCompatActivity {
+    private ImageView cupheadImageView;
+    private ImageView titleImageView;
+    private AnimationDrawable cupheadAnimationDrawable;
+    private AnimationDrawable titleAnimationDrawable;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
+
+        cupheadImageView = findViewById(R.id.cupheadImageView);
+        titleImageView =  findViewById(R.id.titleImageView);
+
+        cupheadAnimationDrawable = (AnimationDrawable) cupheadImageView.getDrawable();
+        cupheadAnimationDrawable.start();
+        titleAnimationDrawable = (AnimationDrawable) titleImageView.getDrawable();
+        titleAnimationDrawable.start();
     }
 }
