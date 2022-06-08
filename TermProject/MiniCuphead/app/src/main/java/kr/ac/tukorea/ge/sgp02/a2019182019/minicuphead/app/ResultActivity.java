38af4158afd555e3b3db6ea85d767c406b2c89d8
gyowoo1113 <yereube@naver.com>
2022-06-08 22:27:07 +0900

    Feat : ResultActivity에서 배경음악 재생

--- TermProject/MiniCuphead/app/src/main/java/kr/ac/tukorea/ge/sgp02/a2019182019/minicuphead/app/ResultActivity.java
+++ TermProject/MiniCuphead/app/src/main/java/kr/ac/tukorea/ge/sgp02/a2019182019/minicuphead/app/ResultActivity.java
@@ -3,6 +3,7 @@ package kr.ac.tukorea.ge.sgp02.a2019182019.minicuphead.app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.drawable.AnimationDrawable;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.widget.ImageView;
 
@@ -14,11 +15,13 @@ public class ResultActivity extends AppCompatActivity {
     private AnimationDrawable cupheadAnimationDrawable;
     private AnimationDrawable titleAnimationDrawable;
 
+    protected MediaPlayer mediaPlayer;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
-
+        playMusic(R.raw.result_bgm);
         cupheadImageView = findViewById(R.id.cupheadImageView);
         titleImageView =  findViewById(R.id.titleImageView);
 
@@ -26,5 +29,14 @@ public class ResultActivity extends AppCompatActivity {
         cupheadAnimationDrawable.start();
         titleAnimationDrawable = (AnimationDrawable) titleImageView.getDrawable();
         titleAnimationDrawable.start();
+    }
+
+    public void playMusic(int resId) {
+        if (mediaPlayer != null) {
+            mediaPlayer.stop();
+        }
+        mediaPlayer = MediaPlayer.create(ResultActivity.this, resId);
+        mediaPlayer.setLooping(true);
+        mediaPlayer.start();
     }
 }
