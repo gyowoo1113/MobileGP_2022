ec47e2d9ede20b6b8ff2124940d209099b689d5a
gyowoo1113 <yereube@naver.com>
2022-06-11 15:44:47 +0900

    Feat #8 : TextView id Rename , findViewById 추가

--- TermProject/MiniCuphead/app/src/main/java/kr/ac/tukorea/ge/sgp02/a2019182019/minicuphead/app/ResultActivity.java
+++ TermProject/MiniCuphead/app/src/main/java/kr/ac/tukorea/ge/sgp02/a2019182019/minicuphead/app/ResultActivity.java
@@ -6,6 +6,7 @@ import android.graphics.drawable.AnimationDrawable;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import kr.ac.tukorea.ge.sgp02.a2019182019.minicuphead.R;
 import kr.ac.tukorea.ge.sgp02.a2019182019.minicuphead.framework.game.GameView;
@@ -19,6 +20,12 @@ public class ResultActivity extends AppCompatActivity {
     private AnimationDrawable titleAnimationDrawable;
     private AnimationDrawable fadeAnimationDrawable;
 
+    private TextView bossTextView;
+    private TextView monTextView;
+    private TextView eventTextView;
+    private TextView hpTextView;
+    private TextView rankTextView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,6 +34,11 @@ public class ResultActivity extends AppCompatActivity {
         cupheadImageView = findViewById(R.id.cupheadImageView);
         titleImageView = findViewById(R.id.titleImageView);
         fadeImageView = findViewById(R.id.fadeImageView);
+        bossTextView = findViewById(R.id.bossTextView);
+        monTextView  = findViewById(R.id.monsterTextView);
+        eventTextView = findViewById(R.id.eventTextView);
+        hpTextView = findViewById(R.id.hpTextView);
+        rankTextView = findViewById(R.id.ScoreTextView);
 
         fadeAnimationDrawable = (AnimationDrawable) fadeImageView.getDrawable();
         fadeAnimationDrawable.start();
